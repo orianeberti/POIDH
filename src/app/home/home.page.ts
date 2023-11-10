c82c648a2b5b415dfe785f24ebb8fef63b1de169
Oriane <oriane@simplecom.fr>
2023-11-10 15:18:10 +0100

    ajout de la modale de commentaire

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  presentingElement: any;
+  message = 'This modal example uses the modalController to present and dismiss modals.';
+  name: string;
 
-  constructor() {}
+  constructor(
+    private modalCtrl: ModalController,
+    private actionSheetCtrl: ActionSheetController
+  ) {}
+
+  ngOnInit() {
+    this.presentingElement = document.querySelector('ion-page');
+  }
+
 
 }
